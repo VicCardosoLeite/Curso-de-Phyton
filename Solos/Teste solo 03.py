49a9e5261ea83f739fac9910686d0428aba43dc2
Victória Cardoso <viihzinha900@gmail.com>
2021-04-27 22:28:46 -0300

    Adicionando os meus solos
    Adicionei os exercícios que fiz por conta própria

---
+++ Solos/Teste solo 03.py
@@ -0,0 +1,10 @@
+print('Olá, antes de prosseguir preciso saber seu nome')
+nome=input('Nome:')
+print('Olá {}, em que posso te ajudar?'.format(nome))
+resposta:input('')
+print('Sorte que sou expert em multiplicação, informe os valores abaixo:')
+n1=int(input('Valor 1:'))
+n2=int(input('Valor 2:'))
+r=n1*n2
+print('O valor é {}, espero ter ajudado'.format(r))
+
