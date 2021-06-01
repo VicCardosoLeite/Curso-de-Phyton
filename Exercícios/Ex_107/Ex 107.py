4310a9f38dfc7bbd73dc516de5c9f83fc0b97d4d
Victória Cardoso <viihzinha900@gmail.com>
2021-05-31 21:50:34 -0300

    Fiz o exercício 107

---
+++ Exercícios/Ex_107/Ex 107.py
@@ -0,0 +1,6 @@
+import Funções_do_107
+valor = float(input('Digite um valor: R$ '))
+print(f'A metade do valor informado é: R${Funções_do_107.metade(valor)}')
+print(f'O dobro do valor informado é: R${Funções_do_107.dobro(valor)} ')
+print(f'Aumentando 10% fica: R${Funções_do_107.aumentar(valor,10)}')
+print(f'E diminuindo 15% fica: R${Funções_do_107.diminuir(valor,15)}')
