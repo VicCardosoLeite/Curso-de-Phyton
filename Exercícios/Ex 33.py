c5a953b6dc696089459234e4cd81bc0b4dee8363
Victória Cardoso <83150019+VictoriaCardosoChavesLeite@users.noreply.github.com>
2022-01-27 22:03:41 -0300

    Update Ex 33.py

--- Exercícios/Ex 33.py
+++ Exercícios/Ex 33.py
@@ -4,7 +4,7 @@ num3 = int(input('Digite mais um valor: '))
 menor = num1
 if num2<num1 and num2<num3:
     menor = num2
-if num3<n1 and num3<num2:
+if num3<num1 and num3<num2:
     menor = num3
 maior = num1
 if num2>num1 and num2>num3:
